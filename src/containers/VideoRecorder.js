54599e84a917811b0a37b0f952a49d08bbac52b7
Gregory Innovo <gregoryinnovo@gmail.com>
2021-05-10 12:19:58 -0500

    History api

--- src/containers/VideoRecorder.js
+++ src/containers/VideoRecorder.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../components/Header/Header';
+import ArduinoConnect from '../components/HistoryVideo/ArduinoConection/ArduinoConnect';
 import CanvasRecorder from '../components/HistoryVideo/CanvasRecorder/CanvasRecorder';
 
 const VideoRecorder = () => {
     return (
         <div>
             <Header />
-            <CanvasRecorder />
+            {/* <CanvasRecorder /> */}
+            <ArduinoConnect />
         </div>
     );
 }
